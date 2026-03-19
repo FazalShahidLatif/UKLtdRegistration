2ccf0774f679f937b523af4f8e868bac245a808f
Your Name <your.email@example.com>
2026-03-19 21:34:40 +0500

    Fix blog router to properly use blogController

--- routes/blog.js
+++ routes/blog.js
@@ -1,21 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const blogController = require('../controllers/blogController');
 
 // Blog listing page
-router.get('/', (req, res) => {
-    res.render('pages/blog-list', {
-        title: 'Business Insights & Formation Guides',
-        metaDescription: 'Expert advice on UK company formation, SIC codes, banking, and business growth.'
-    });
-});
+router.get('/', blogController.index);
 
 // Single blog post
-router.get('/:slug', (req, res) => {
-    res.render('pages/blog-single', {
-        title: 'Blog Post', // This would normally come from a DB
-        slug: req.params.slug,
-        metaDescription: 'Read our latest insights'
-    });
-});
+router.get('/:slug', blogController.show);
 
 module.exports = router;
